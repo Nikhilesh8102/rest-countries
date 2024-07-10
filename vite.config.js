9075dee130501f95da8ab1d4fd21903e16fe9572
Nikhilesh Ravulapelli <sumu8224@gmail.com>
2024-07-10 15:12:28 +0530

    add vercel config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true, // Ensures client-side routing works during development
+  },
+  build: {
+    outDir: 'dist', // Default output directory, change if necessary
+  },
 })
